3287cb32596d7aaa9a04f0593bd291ff3712b8fc
Luca Hagel <hagel.luca@gmail.com>
2017-01-30 20:04:12 -0800

    add frequency funtion

--- practice/histogram.py
+++ practice/histogram.py
@@ -12,10 +12,18 @@ def generate_histogram(text):
     return histogram
 
 def unique_words(histogram):
-    """takes a histgram and return the number of unique words"""
+    """takes a histgram and returns the number of unique words"""
     return len(histogram)
+
+def frequency(histogram, word):
+    """takes a histgram and a word and returns the number of times the word appeared"""
+    if word in histogram:
+        return histogram[word]
+    else:
+        return 0
 
 if __name__ == '__main__':
     FISH = generate_histogram("one fish two fish red fish blue fish")
     print FISH
     print unique_words(FISH)
+    print frequency(FISH, 'fish')
