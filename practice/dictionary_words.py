f4d88e3bd077a92b43c54f91943016b9de70b5cd
Luca Hagel <hagel.luca@gmail.com>
2017-01-31 13:49:59 -0800

    update dictionary_words to only fetch words once

--- practice/dictionary_words.py
+++ practice/dictionary_words.py
@@ -12,17 +12,16 @@ def get_words():
         words += [line]
     return words
 
-def generate_sentence(length):
+def generate_sentence(words, length):
     """generates a sentence of the passed in length"""
-    words = get_words()
     words_count = len(words)
-
     sentence = ""
-    for i in range(0, length):
+    for _ in range(0, length):
         index = random.randint(0, words_count - 1)
         sentence += words[index].rstrip() + ' '
     return sentence
 
 if __name__ == '__main__':
-    print generate_sentence(int(sys.argv[1]))
+    WORDS = get_words()
+    print generate_sentence(WORDS, int(sys.argv[1]))
     print "Generating the sentence took " + str(datetime.now() - START_TIME)
