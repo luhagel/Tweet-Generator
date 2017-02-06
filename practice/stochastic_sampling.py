4161e31b34877052f71495fd798c088138424f10
Luca Hagel <hagel.luca@gmail.com>
2017-02-05 21:56:53 -0800

    Implement weighted probability

--- practice/stochastic_sampling.py
+++ practice/stochastic_sampling.py
@@ -15,6 +15,8 @@ def generate_histogram(text):
 
 ############################################
 
+WORDS_COUNT = 0
+
 def pick_random_word(arr):
     """takes an array, shuffles it and return the first element"""
     random.shuffle(arr)
@@ -26,12 +28,36 @@ def get_frequency(arr):
     text = ' '.join(arr)
     hist = generate_histogram(text)
     for key in hist:
-        print hist[key] / float(len(arr))
+        print  key + ' -> ' + str(hist[key] / float(len(arr)))
 
+def get_words_count(dict):
+    count = 0
+    for key in dict:
+        count += dict[key]
+    return count
+
+def pick_weighted_random_word(dict):
+    index = random.randint(0, WORDS_COUNT)
+    count = 0
+    for key in dict:
+        count += dict[key]
+        if count >= index:
+            return key    
 
 
 if __name__ == '__main__':
     WORDS = []
+    WEIGHTED_WORDS = []
+
+    INPUT_TEXT = ' '.join(sys.argv[1:])
+    INPUT_HISTOGRAM = generate_histogram(INPUT_TEXT)
+    WORDS_COUNT = get_words_count(INPUT_HISTOGRAM)
+    print WORDS_COUNT
+
     for i in range(0, 10000):
         WORDS.append(pick_random_word(sys.argv[1:]))
     get_frequency(WORDS)
+    print '--------------------------------------------'
+    for i in range(0, 10000):
+        WEIGHTED_WORDS.append(pick_weighted_random_word(INPUT_HISTOGRAM))
+    get_frequency(WEIGHTED_WORDS)
